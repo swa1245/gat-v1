d554f09bc4be4dbf9c1f8ef4805b4e7f2ff5d0b1
swa1245 <swarajpatil2123@gmail.com>
2025-05-08 23:31:41 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="fixed top-0 flex items-center justify-center left-0 z-[100] w-full h-screen overflow-hidden bg-black">
+      <svg viewBox="0 0 800 600" preserveAspectRatio="xMidYMid slice">
+        <defs>
+          <mask id="viMask">
+            <rect width="100%" height="100%" fill="black" />
+            <g className="vi-mask-group">
+              <text
+                x="50%"
+                y="50%"
+                fontSize="250"
+                textAnchor="middle"
+                fill="white"
+                dominantBaseline="middle"
+                fontFamily="Arial Black"
+              >
+                VI
+              </text>
+            </g>
+          </mask>
+        </defs>
+        <image
+          href="./bg.png"
+          width="100%"
+          height="100%"
+          preserveAspectRatio="xMidYMid slice"
+          mask="url(#viMask)"
+        />
+      </svg>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
